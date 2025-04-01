13ff271989f0a79bd19085c4a845c0ea76ea7a39
minotaa <jpichardo369@gmail.com>
2025-04-01 11:28:01 -0400

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { injectAnalytics } from '@vercel/analytics/sveltekit'
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<div class="md:container pl-8 pt-8 pr-4">
+  <h1 class="font-departure text-4xl text-white font-bold">Bulletlane</h1>
+  <p class="font-departure text-lg text-white">Bulletlane is a fast paced highway shoot 'em up. Collect coins and powerups, defeat enemies and upgrade your cars to progress in this game. This game was created by <a target="_blank" href="https://minota.cc" class="transition-colors text-sky-500 hover:text-sky-600">Juan Pichardo</a>.</p>
+
+  <h2 class="font-departure text-3xl text-white mt-6 mb-4">Important Links</h2>
+  <div class="flex md:flex-row flex-col md:gap-4 gap-2">
+    <a target="_blank" href="https://apps.apple.com/us/app/mirkwood/id6738539616" class="border-8 border-white pt-4 pb-4 pl-8 pr-8 text-xl font-sink text-white hover:bg-zinc-800 transition-colors">App Store</a>
+    <a target="_blank" href="/privacy" class="border-8 border-white pt-4 pb-4 pl-8 pr-8 text-xl font-sink text-white hover:bg-zinc-800 transition-colors">Privacy Policy</a>
+    <a target="_blank" href="https://discord.gg/MZhUHZnt6y" class="border-8 border-white pt-4 pb-4 pl-8 pr-8 text-xl font-sink text-white hover:bg-zinc-800 transition-colors">Support Server</a>
+  
+  </div>
+</div>
