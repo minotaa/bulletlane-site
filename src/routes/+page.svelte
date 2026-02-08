5624dc6381484d805a2caf156a9b589778c7fd1c
minota <jpichardo369@gmail.com>
2026-02-08 16:18:07 -0500

    Add Google Play Store link to page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,8 @@
   <h2 class="font-departure text-3xl text-white mt-6 mb-4">Important Links</h2>
   <div class="flex md:flex-row flex-col md:gap-4 gap-2">
     <a target="_blank" href="https://apps.apple.com/us/app/bulletlane/id6743356289" class="border-8 border-white pt-4 pb-4 pl-8 pr-8 text-xl font-sink text-white hover:bg-zinc-800 transition-colors">App Store</a>
+    <a target="_blank" href="https://play.google.com/store/apps/details?id=cc.minota.road" class="border-8 border-white pt-4 pb-4 pl-8 pr-8 text-xl font-sink text-white hover:bg-zinc-800 transition-colors">Google Play Store</a>
     <a target="_blank" href="/privacy" class="border-8 border-white pt-4 pb-4 pl-8 pr-8 text-xl font-sink text-white hover:bg-zinc-800 transition-colors">Privacy Policy</a>
     <a target="_blank" href="https://discord.gg/MZhUHZnt6y" class="border-8 border-white pt-4 pb-4 pl-8 pr-8 text-xl font-sink text-white hover:bg-zinc-800 transition-colors">Support Server</a>
-  
   </div>
 </div>
